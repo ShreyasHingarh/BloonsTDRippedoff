8b30218a0387feee69b953aff534315b577c56f3
shreyas.hingarh2@gmail.com <shreyas.hingarh2@gmail.com>
2023-07-22 17:32:05 -0700

    Started it

---
+++ New folder/MapEditor/MapEditor/Editor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor
+{
+    internal class Editor
+    {
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+    }
+}
