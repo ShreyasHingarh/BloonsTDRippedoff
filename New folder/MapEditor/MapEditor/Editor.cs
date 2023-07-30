1c4a558d05a71f6a7274c7b6dd02ae79b70a75f0
shreyas.hingarh <shreyas.hingarh@GMR-118-2-3>
2023-07-29 17:33:20 -0700

    Made a editor need to have some way of saving it

--- New folder/MapEditor/MapEditor/Editor.cs
+++ New folder/MapEditor/MapEditor/Editor.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +12,95 @@ namespace MapEditor
 {
     internal class Editor
     {
-        public void Update()
+        public List<Sprite> NewPaths;
+        bool hasAdded = false;
+        public Editor()
         {
+            NewPaths = new List<Sprite>();
+        }
+        public void Update(Sprite Path,GraphicsDevice graphics, Sprite[] Grass)
+        {
+            var mouseState = Mouse.GetState();
+            var mousePos = new Point(mouseState.X, mouseState.Y);
+            if (mousePos.X < 0)
+            {
+                Mouse.SetPosition(0, mousePos.Y);
+            }
+            if (mousePos.X > graphics.Viewport.Width)
+            {
+                Mouse.SetPosition(graphics.Viewport.Width, mousePos.Y);
+            }
+            if (mousePos.Y < 0)
+            {
+                Mouse.SetPosition(mousePos.X, 0);
+            }
+            if (mousePos.Y > graphics.Viewport.Height)
+            {
+                Mouse.SetPosition(mousePos.X, graphics.Viewport.Height);
+            }
+            mousePos = new Point(mouseState.X, mouseState.Y);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.E))
+            {
+                foreach(var path in NewPaths)
+                {
+                    if(path.HitBox.Value.Contains(mousePos))
+                    {
+                        NewPaths.Remove(path);
+                        break;
+                    }
+                }
+            }
+            else
+            {   
+                if (mouseState.LeftButton == ButtonState.Pressed && (Path.HitBox.Value.Contains(mousePos) || hasAdded))
+                {
+                    if (hasAdded == false)
+                    {
+                        NewPaths.Add(new Sprite(Color.White, new Vector2(mousePos.X, mousePos.Y), Path.Image, 0, Vector2.Zero, Vector2.One));
+                        hasAdded = true;
+                    }
+                    else if (hasAdded)
+                    {
+                        NewPaths[NewPaths.Count - 1].Position = new Vector2(mousePos.X, mousePos.Y);
+                    }
+                }
+                else if (mouseState.LeftButton == ButtonState.Released && hasAdded)
+                {
+                    if(NewPaths.Count != 0)
+                    {
+                        foreach (var item in NewPaths)
+                        {
+                            foreach (var actual in NewPaths)
+                            {
+                                if (item == actual) continue;
+                                if (item.HitBox.Value.Contains(actual.HitBox.Value))
+                                {
+                                    NewPaths.Remove(actual);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    
+                    foreach (var grassSquare in Grass)
+                    {
+                        if (grassSquare.HitBox.Value.Contains(NewPaths[NewPaths.Count - 1].Position))
+                        {
+                            NewPaths[NewPaths.Count - 1].Position = grassSquare.Position;
+                        }
+                    }
+                    hasAdded = false;
+                }
+            }
 
         }
-        public void Draw()
+        public void Draw(SpriteBatch spriteBatch)
         {
-
+            foreach(var item in NewPaths)
+            {
+                item.Draw(spriteBatch);
+            }
         }
     }
 }
--- New folder/MapEditor/MapEditor/Game1.cs
+++ New folder/MapEditor/MapEditor/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
+using System.Net;
 
 namespace MapEditor
 {
@@ -8,7 +11,11 @@ namespace MapEditor
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-
+        Editor editor;
+        Sprite SquarePath;
+        Sprite Start;
+        Sprite End;
+        Sprite[] Grass;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -19,14 +26,33 @@ namespace MapEditor
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            
+            editor = new Editor();
+            int count = 0;
+            Texture2D grass = Content.Load<Texture2D>("Grass");
+            int x = GraphicsDevice.Viewport.Height /grass.Width;
+            int y = GraphicsDevice.Viewport.Height / grass.Height;
+            Grass = new Sprite[x*y];
+            for (int i = 0;i < y;i++)
+            {
+                for (int z = 0; z < x; z++)
+                {
+                    Grass[count] = new Sprite(Color.White,new Vector2(grass.Width*z,grass.Height*i),grass,0,Vector2.Zero,Vector2.One);
+                    count++;
+                }
+            }
+            Start = new Sprite(Color.White, new Vector2(700,0),Content.Load<Texture2D>("Start"),0,Vector2.Zero,Vector2.One);
+            End = new Sprite(Color.White, new Vector2(650, 0), Content.Load<Texture2D>("End"), 0, Vector2.Zero, Vector2.One);
+            SquarePath = new Sprite(Color.White, new Vector2(650, 450), Content.Load<Texture2D>("Path"), 0, Vector2.Zero,Vector2.One);
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,17 +62,29 @@ namespace MapEditor
                 Exit();
 
             // TODO: Add your update logic here
-
+            editor.Update(SquarePath,GraphicsDevice,Grass);
+            editor.Update(Start, GraphicsDevice, Grass);
+            editor.Update(End, GraphicsDevice, Grass);
+            SquarePath.Position = new Vector2(650,450);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            spriteBatch.Begin(); 
+            SquarePath.Draw(spriteBatch);
+            Start.Draw(spriteBatch);
+            End.Draw(spriteBatch);
+            foreach (var item in Grass)
+            {
+                item.Draw(spriteBatch);
+            }
+            editor.Draw(spriteBatch);
             // TODO: Add your drawing code here
-
+            
             base.Draw(gameTime);
+            spriteBatch.End();
         }
     }
 }
